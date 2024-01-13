0bc2128c204362bef8a162bc64a801591ea5f17a
Venkat.R <ramsunvtech@gmail.com>
2024-01-13 23:51:26 +0800

    react: add root index

---
+++ index.js
@@ -0,0 +1,10 @@
+// Root index.js in TwixtUI package
+
+// Re-exporting React components
+export * as ReactComponents from './react';
+
+// Similarly, you can re-export components for Angular and Vue.js
+// when they are available in your package.
+
+// export * as AngularComponents from './angular';
+// export * as VueComponents from './vue';
