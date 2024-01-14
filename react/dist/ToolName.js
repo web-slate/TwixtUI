f1ed1327dae1c05cff5fe427b4fdc3362514a0ea
Venkat.R <ramsunvtech@gmail.com>
2024-01-14 20:00:11 +0800

    tech(build): add dist for Textbox and ToolName

---
+++ react/dist/ToolName.js
@@ -0,0 +1,33 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var ToolName = function ToolName(_ref) {
+  var name = _ref.name,
+    status = _ref.status,
+    emoji = _ref.emoji;
+  var getStatusEmoji = function getStatusEmoji() {
+    switch (status) {
+      case 'down':
+        return '🔻';
+      case 'deprecated':
+        return '⚠️';
+      case 'christmas':
+        return '🎄';
+      case 'newyear':
+        return '🎆';
+      default:
+        return '';
+    }
+  };
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    className: "flex items-center space-x-2 font-special"
+  }, /*#__PURE__*/_react["default"].createElement("span", null, name), getStatusEmoji() && /*#__PURE__*/_react["default"].createElement("span", null, getStatusEmoji()), emoji && /*#__PURE__*/_react["default"].createElement("span", null, emoji), /*#__PURE__*/_react["default"].createElement("span", {
+    className: "text-xs text-gray-500"
+  }, "Powered by Tailwind"));
+};
+var _default = exports["default"] = ToolName;
---
+++ react/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "InputSingleLine", {
+  enumerable: true,
+  get: function get() {
+    return _InputSingleLine["default"];
+  }
+});
+var _InputSingleLine = _interopRequireDefault(require("./InputSingleLine"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
