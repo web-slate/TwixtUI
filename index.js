acc95010c2adde7b08cb26d59b1786668bb93cd5
Venkat.R <ramsunvtech@gmail.com>
2024-01-14 19:55:44 +0800

    tech(clean-up): root files removal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Root index.js in TwixtUI package
 
 // Re-exporting React components
-export * as ReactComponents from './react';
+export * as ReactComponents from './react/dist';
 
 // Similarly, you can re-export components for Angular and Vue.js
 // when they are available in your package.
