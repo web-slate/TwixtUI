3fcf405d7571a94744fe9012a88f10bf03afcadb
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 18:34:56 +0800

    button: update overwrite class props

--- react/src/Button.js
+++ react/src/Button.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-export default function TwixtButton({ children, onClick }) {
+export default function TwixtButton({ children, onClick, overwriteClass }) {
+  const buttonClasses = overwriteClass ? overwriteClass : 'px-4 py-2 bg-indigo-500 text-white rounded-md';
+
   return (
-    <button onClick={onClick} className="px-4 py-2 bg-indigo-500 text-white rounded-md">
+    <button onClick={onClick} className={buttonClasses}>
       {children}
     </button>
   );
