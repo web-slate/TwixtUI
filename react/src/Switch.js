c491b7d89d947a4e72632292b6a0f53964dfc80d
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 19:05:58 +0800

    switch: update label, onChange props

--- react/src/Switch.js
+++ react/src/Switch.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 
-const TwixtSwitch = () => {
+const TwixtSwitch = ({ label, onChange }) => {
   const [isOn, setIsOn] = useState(false);
 
   const toggleSwitch = () => {
     setIsOn(!isOn);
+    onChange(!isOn);
   };
 
   return (
     <>
+      <label htmlFor="toggle" className="text-xs text-gray-700">{label}</label>
       <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
         <input type="checkbox" name="toggle" id="toggle" className="twixt-toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" checked={isOn}
           onChange={toggleSwitch} />
         <label htmlFor="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
       </div>
-      <label htmlFor="toggle" className="text-xs text-gray-700">Toggle me.</label>
     </>
   );
 };
