9104cbf143b8a77d1bce376fad71f1e3b151a206
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 18:52:08 +0800

    skeleton: updated overwrite class props

--- react/src/Skeleton.js
+++ react/src/Skeleton.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
-export function LeftSideBar({ canOpen = false }) {
+export function LeftSideBar({ overWriteClasses, canOpen = false }) {
   const [isOpen, setIsOpen] = useState(canOpen);
 
+  const leftSidebarClass = overWriteClasses ? overWriteClasses : 'flex w-72 h-full bg-pink-500';
+
   return (
-    <nav className="flex w-72 h-full bg-pink-500">
+    <nav className={leftSidebarClass}>
       <div className="w-full flex mx-auto px-6 py-8">
         <div className="flex">
           {/* Overlay */}
@@ -40,27 +42,26 @@ export function LeftSideBar({ canOpen = false }) {
   );
 }
 
-export function RightSideBar() {
+export function RightSideBar({ overWriteClasses, children }) {
+  const rightSidebarClass = overWriteClasses ? overWriteClasses : 'flex w-72 h-full bg-yellow-400';
+
   return (
-    <nav className="flex w-72 h-full bg-yellow-400">
+    <nav className={rightSidebarClass}>
       <div className="w-full flex mx-auto px-6 py-8">
-        <div className="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Rightbar</div>
+        <div className="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900">{children}</div>
       </div>
     </nav>
   );
 }
 
-export function ContentPane({ children }) {
+export function ContentPane({ overWriteClasses, children }) {
+  const contentPaneClass = overWriteClasses ? overWriteClasses : 'flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14';
+
   return (
-    <main className="flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14">
+    <main className={contentPaneClass}>
       <div className="flex w-full mx-auto px-6 py-8">
         <div className="flex flex-col w-full h-full text-gray-900 text-xl border-4 border-gray-900 border-dashed">
           {children}
-          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
-          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
-          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
-          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
-          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
         </div>
       </div>
     </main>
