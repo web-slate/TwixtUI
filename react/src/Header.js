d2f74c3742c54e835d8de1d2ce574bc11dbb0505
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 21:17:06 +0800

    header: onHamburgerClick props

--- react/src/Header.js
+++ react/src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export default function TwixtHeader({ headerClass, children, rightBlock }) {
+const noop = () => {};
+
+export default function TwixtHeader({ headerClass, children, rightBlock, onHamburgerClick = noop }) {
   return (
     <header className={`flex justify-between items-center ${headerClass} p-4`}>
       <div className="flex">
         <button
-          className="bg-slate-800 text-slate-100 rounded-full pr-2">
+          className="bg-slate-800 text-slate-100 rounded-full pr-2" onClick={onHamburgerClick}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
             stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLineCap="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
