d089dca0ecef9598b7bb7a13880e8ba3e49130b4
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 17:22:58 +0800

    build: table fix

--- react/src/SimpleTable.js
+++ react/src/SimpleTable.js
@@ -1,5 +1,22 @@
 import React, { useState } from 'react';
 
+const TableHeader = () => {
+  return (
+    <div className="px-4 md:px-10 py-4 md:py-7">
+      <div className="flex items-center justify-between">
+        <p className="focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">Tasks</p>
+        <div className="py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 hover:bg-gray-300 cursor-pointer rounded">
+          <p>Sort By:</p>
+          <select aria-label="select" className="focus:text-indigo-600 focus:outline-none bg-transparent ml-1">
+            <option className="text-sm text-indigo-800">Latest</option>
+            <option className="text-sm text-indigo-800">Oldest</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const TableFilters = () => {
   return (
     <div className="sm:flex items-center justify-between">
