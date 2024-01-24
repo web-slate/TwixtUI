cb7c14c31cca2672d383b561aa35b8f938a2afac
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 18:05:59 +0800

    build: renamed without twixt

--- react/dist/index.js
+++ react/dist/index.js
@@ -3,40 +3,10 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "CommentsSystem", {
-  enumerable: true,
-  get: function get() {
-    return _CommentsSystem["default"];
-  }
-});
-Object.defineProperty(exports, "Header", {
-  enumerable: true,
-  get: function get() {
-    return _Header["default"];
-  }
-});
-Object.defineProperty(exports, "Heading", {
-  enumerable: true,
-  get: function get() {
-    return _Heading["default"];
-  }
-});
 Object.defineProperty(exports, "InputSingleLine", {
   enumerable: true,
   get: function get() {
     return _InputSingleLine["default"];
-  }
-});
-Object.defineProperty(exports, "LoadingPostCard", {
-  enumerable: true,
-  get: function get() {
-    return _LoadingPostCard["default"];
-  }
-});
-Object.defineProperty(exports, "Modal", {
-  enumerable: true,
-  get: function get() {
-    return _Modal["default"];
   }
 });
 Object.defineProperty(exports, "SimpleTable", {
@@ -45,40 +15,70 @@ Object.defineProperty(exports, "SimpleTable", {
     return _SimpleTable["default"];
   }
 });
-Object.defineProperty(exports, "Skeleton", {
-  enumerable: true,
-  get: function get() {
-    return _Skeleton["default"];
-  }
-});
-Object.defineProperty(exports, "Switch", {
-  enumerable: true,
-  get: function get() {
-    return _Switch["default"];
-  }
-});
-Object.defineProperty(exports, "ToolName", {
-  enumerable: true,
-  get: function get() {
-    return _ToolName["default"];
-  }
-});
 Object.defineProperty(exports, "TwixtButton", {
   enumerable: true,
   get: function get() {
-    return _TwixtButton["default"];
+    return _Button["default"];
   }
 });
 Object.defineProperty(exports, "TwixtCheckbox", {
   enumerable: true,
   get: function get() {
-    return _TwixtCheckbox["default"];
+    return _Checkbox["default"];
+  }
+});
+Object.defineProperty(exports, "TwixtCommentsSystem", {
+  enumerable: true,
+  get: function get() {
+    return _CommentsSystem["default"];
+  }
+});
+Object.defineProperty(exports, "TwixtHeader", {
+  enumerable: true,
+  get: function get() {
+    return _Header["default"];
+  }
+});
+Object.defineProperty(exports, "TwixtHeading", {
+  enumerable: true,
+  get: function get() {
+    return _Heading["default"];
+  }
+});
+Object.defineProperty(exports, "TwixtLoadingPostCard", {
+  enumerable: true,
+  get: function get() {
+    return _LoadingPostCard["default"];
+  }
+});
+Object.defineProperty(exports, "TwixtModal", {
+  enumerable: true,
+  get: function get() {
+    return _Modal["default"];
+  }
+});
+Object.defineProperty(exports, "TwixtSkeleton", {
+  enumerable: true,
+  get: function get() {
+    return _Skeleton["default"];
+  }
+});
+Object.defineProperty(exports, "TwixtSwitch", {
+  enumerable: true,
+  get: function get() {
+    return _Switch["default"];
   }
 });
 Object.defineProperty(exports, "TwixtTextArea", {
   enumerable: true,
   get: function get() {
-    return _TwixtTextArea["default"];
+    return _TextArea["default"];
+  }
+});
+Object.defineProperty(exports, "TwixtToolName", {
+  enumerable: true,
+  get: function get() {
+    return _ToolName["default"];
   }
 });
 var _InputSingleLine = _interopRequireDefault(require("./InputSingleLine"));
@@ -90,8 +90,8 @@ var _LoadingPostCard = _interopRequireDefault(require("./LoadingPostCard"));
 var _Modal = _interopRequireDefault(require("./Modal"));
 var _Switch = _interopRequireDefault(require("./Switch"));
 var _Skeleton = _interopRequireDefault(require("./Skeleton"));
-var _TwixtButton = _interopRequireDefault(require("./TwixtButton"));
-var _TwixtTextArea = _interopRequireDefault(require("./TwixtTextArea"));
-var _TwixtCheckbox = _interopRequireDefault(require("./TwixtCheckbox"));
+var _Button = _interopRequireDefault(require("./Button"));
+var _TextArea = _interopRequireDefault(require("./TextArea"));
+var _Checkbox = _interopRequireDefault(require("./Checkbox"));
 var _SimpleTable = _interopRequireDefault(require("./SimpleTable"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
