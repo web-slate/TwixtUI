8e62bd25f1494c0c9dc0ca71497ecd8af02a5891
Venkat.R <ramsunvtech@gmail.com>
2024-01-27 15:11:38 +0800

    alert: add simple alert

---
+++ react/src/Alert.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TwixtAlert({ overwriteClass, title, desc }) {
+  const alertClasses = overwriteClass ? overwriteClass : 'bg-orange-100 border-l-4 border-orange-500 text-orange-700 mx-4 p-2';
+
+  return (
+    <div class={alertClasses} role="alert">
+      <p class="font-bold">{title}</p>
+      <p>{desc}</p>
+    </div>
+  );
+}
