32925ac930a23caa7851a67a15e8a5f18b2e83bc
Venkat.R <ramsunvtech@gmail.com>
2024-01-27 15:36:48 +0800

    header: add alert as announcement info

--- react/dist/Header.js
+++ react/dist/Header.js
@@ -5,15 +5,20 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = TwixtHeader;
 var _react = _interopRequireDefault(require("react"));
+var _Alert = _interopRequireDefault(require("./Alert"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var noop = function noop() {};
 function TwixtHeader(_ref) {
-  var headerClass = _ref.headerClass,
+  var announceMent = _ref.announceMent,
+    headerClass = _ref.headerClass,
     children = _ref.children,
     rightBlock = _ref.rightBlock,
     _ref$onHamburgerClick = _ref.onHamburgerClick,
     onHamburgerClick = _ref$onHamburgerClick === void 0 ? noop : _ref$onHamburgerClick;
-  return /*#__PURE__*/_react["default"].createElement("header", {
+  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, announceMent && /*#__PURE__*/_react["default"].createElement(_Alert["default"], {
+    title: announceMent.title,
+    desc: announceMent.desc
+  }), /*#__PURE__*/_react["default"].createElement("header", {
     className: "flex justify-between items-center ".concat(headerClass, " p-4")
   }, /*#__PURE__*/_react["default"].createElement("div", {
     className: "flex"
@@ -33,5 +38,5 @@ function TwixtHeader(_ref) {
     d: "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
   }))), children), /*#__PURE__*/_react["default"].createElement("div", {
     className: "flex"
-  }, rightBlock));
+  }, rightBlock)));
 }
