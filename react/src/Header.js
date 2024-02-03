367b86c7bcbcf421ada82fa05454cf0db7d02ed4
Venkat.R <ramsunvtech@gmail.com>
2024-02-03 13:55:03 +0800

    react: format header

--- react/src/Header.js
+++ react/src/Header.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import TwixtAlert from './Alert'
 
-const noop = () => {};
+const noop = () => { };
 
 export default function TwixtHeader({ announceMent, headerClass, children, rightBlock, onHamburgerClick = noop }) {
   return (
     <>
-    {announceMent && (
-      <TwixtAlert title={announceMent.title} desc={announceMent.desc} />
-    )}
-    <header className={`flex justify-between items-center ${headerClass} p-4`}>
-      <div className="flex">
-        <button
-          className="bg-slate-800 text-slate-100 rounded-full pr-2" onClick={onHamburgerClick}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-            stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLineCap="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-        </button>
-        {children}
-      </div>
-      <div className="flex">{rightBlock}</div>
-    </header>
-              </>
+      {announceMent && (
+        <TwixtAlert title={announceMent.title} desc={announceMent.desc} />
+      )}
+      <header className={`flex justify-between items-center ${headerClass} p-4`}>
+        <div className="flex">
+          <button
+            className="bg-slate-800 text-slate-100 rounded-full pr-2" onClick={onHamburgerClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+              stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLineCap="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+          {children}
+        </div>
+        <div className="flex">{rightBlock}</div>
+      </header>
+    </>
   );
 }
