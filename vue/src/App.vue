74a2977ff75eafe166661235a2c36a2960529fd7
Venkat.R <ramsunvtech@gmail.com>
2024-03-18 06:14:38 +0800

    vue: add source

---
+++ vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>TwixtUI for Vue</h1>
+  <InputSingleLine msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import InputSingleLine from './components/InputSingleLine.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InputSingleLine
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
