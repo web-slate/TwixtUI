e3bc4adbb1d1953150b439b484ba2c0817db56ea
Venkat.R <ramsunvtech@gmail.com>
2024-04-18 21:52:35 +0800

    update heading build

--- react/dist/Heading.js
+++ react/dist/Heading.js
@@ -1,48 +1,40 @@
 "use strict";
 
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = Header;
-var _react = _interopRequireWildcard(require("react"));
-var _react2 = require("TwixtUI/react");
-function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
-function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
-function TwixtHeader(_ref) {
-  var children = _ref.children;
-  var headingContainerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-    //,
-    //height: '100vh', // Optionally, set a height for vertical centering    
-  };
-  var centerHeadingStyle = {
-    textAlign: 'center'
-    // Add any additional styling you may need for your headings
-  };
-  return /*#__PURE__*/_react["default"].createElement("header", {
-    className: "flex justify-between items-center bg-blue-300 p-4"
-  }, /*#__PURE__*/_react["default"].createElement("div", {
-    style: headingContainerStyle
-  }, /*#__PURE__*/_react["default"].createElement("h1", {
-    style: centerHeadingStyle
-  }, "Heading 1"), /*#__PURE__*/_react["default"].createElement("h2", {
-    style: centerHeadingStyle
-  }, "Heading 2"), /*#__PURE__*/_react["default"].createElement("h3", {
-    style: centerHeadingStyle
-  }, "Heading 3"), /*#__PURE__*/_react["default"].createElement("h4", {
-    style: centerHeadingStyle
-  }, "Heading 4"), /*#__PURE__*/_react["default"].createElement("h5", {
-    style: centerHeadingStyle
-  }, "Heading 5"), /*#__PURE__*/_react["default"].createElement("h6", {
-    style: centerHeadingStyle
-  }, "Heading 6")));
-}
-function Header() {
-  return /*#__PURE__*/_react["default"].createElement(TwixtHeader, null, /*#__PURE__*/_react["default"].createElement(_react2.ToolName, {
-    name: "Twixt UI React App"
-  }));
+exports["default"] = TwixtHeading;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function TwixtHeading(_ref) {
+  var _ref$type = _ref.type,
+    type = _ref$type === void 0 ? "h6" : _ref$type,
+    children = _ref.children;
+  switch (type) {
+    case 'h1':
+      return /*#__PURE__*/_react["default"].createElement("h1", {
+        className: "text-4xl font-bold text-gray-900"
+      }, children);
+    case 'h2':
+      return /*#__PURE__*/_react["default"].createElement("h2", {
+        className: "text-3xl font-bold text-gray-800"
+      }, children);
+    case 'h3':
+      return /*#__PURE__*/_react["default"].createElement("h3", {
+        className: "text-2xl font-bold text-gray-700"
+      }, children);
+    case 'h4':
+      return /*#__PURE__*/_react["default"].createElement("h4", {
+        className: "text-xl font-semibold text-gray-600"
+      }, children);
+    case 'h5':
+      return /*#__PURE__*/_react["default"].createElement("h5", {
+        className: "text-lg font-medium text-gray-500"
+      }, children);
+    case 'h6':
+      return /*#__PURE__*/_react["default"].createElement("h6", {
+        className: "text-base font-normal text-gray-400"
+      }, children);
+  }
+  return /*#__PURE__*/_react["default"].createElement("h1", null, children);
 }
