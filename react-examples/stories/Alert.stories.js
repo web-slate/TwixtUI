3a2fa7d9f685fb83c0beb8ce9490b32026424836
Venkat.R <ramsunvtech@gmail.com>
2024-04-27 18:41:10 +0800

    add alert documentation

--- react-examples/stories/Alert.stories.js
+++ react-examples/stories/Alert.stories.js
@@ -1,6 +1,5 @@
 import { fn } from '@storybook/test';
 import TwixtAlert from '../../react/src/Alert.js';
-import '../../react/dist/tailwind.css';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
@@ -23,30 +22,55 @@ export default {
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary = {
+export const Announcement = {
   args: {
-    title: 'Welcome to Twixt React Alert Component',
+    title: 'Welcome',
     desc: 'Explore the Alert for improved layout designed to enhance your experience.'
   },
 };
 
-export const Secondary = {
+export const ErrorNotice = {
   args: {
-    label: 'Service Disruption Notice',
+    title: 'Notice',
     desc: 'We are currently experiencing technical difficulties with our app. Please be patient as we work to resolve this issue.',
   },
 };
 
-export const Large = {
+export const Notification = {
   args: {
-    label: 'Urgent Security Notification',
+    title: 'Urgent Security Notification',
     desc: 'We have detected unusual activity in some user accounts. Please reset your password immediately if you notice any suspicious activity.',
   },
 };
 
-export const Small = {
+export const IncreasePadding = {
   args: {
-    label: 'Small',
-    desc: 'Button',
+    title: 'Increase Padding Title',
+    desc: 'Desc goes here',
+    overwriteClass: 'bg-orange-100 border-l-4 border-orange-500 text-orange-700 mx-4 p-7'
+  },
+};
+
+export const BackgroundChange = {
+  args: {
+    title: 'Change Background color',
+    desc: 'Desc goes here',
+    overwriteClass: 'bg-yellow-100 border-l-4 border-orange-500 text-orange-700 mx-4 p-2'
+  },
+};
+
+export const BorderColorChange = {
+  args: {
+    title: 'Change Border Change',
+    desc: 'Desc goes here',
+    overwriteClass: 'bg-orange-100 border-l-4 border-blue-500 text-orange-700 mx-4 p-2'
+  },
+};
+
+export const TextColorChange = {
+  args: {
+    title: 'Change Text Color',
+    desc: 'Desc goes here',
+    overwriteClass: 'bg-orange-100 border-l-4 border-orange-500 text-black-700 mx-4 p-2'
   },
 };
