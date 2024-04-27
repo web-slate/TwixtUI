19b2cc41a4c022af014f0e69a81b1dac7e3701fa
Venkat.R <ramsunvtech@gmail.com>
2024-04-27 18:15:44 +0800

    Update Alert.js for fix

--- react/src/Alert.js
+++ react/src/Alert.js
@@ -4,8 +4,8 @@ export default function TwixtAlert({ overwriteClass, title, desc }) {
   const alertClasses = overwriteClass ? overwriteClass : 'bg-orange-100 border-l-4 border-orange-500 text-orange-700 mx-4 p-2';
 
   return (
-    <div class={alertClasses} role="alert">
-      <p class="font-bold">{title}</p>
+    <div className={alertClasses} role="alert">
+      <p className="font-bold">{title}</p>
       <p>{desc}</p>
     </div>
   );
