18256039d6a77fe8ece568b069ddcc71dda0f177
adhiththiyan <adhiththiyan07@example.com>
2024-05-04 10:16:12 +0530

    added no result component

---
+++ react/src/NoResult.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoResult = () => {
+  return (
+    <div className="flex items-center justify-center h-40">
+        <p className="text-gray-950 ffont-normal">No Result Found.</p>
+    </div>
+  )
+}
+
+export default NoResult
