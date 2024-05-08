caab7e0657d21ad9c6d844f0bd97088b0e846bb0
sriraman <sriramanvellingiri@gmail.com>
2024-05-08 18:40:30 +0530

    fix: add copy button story book

---
+++ react-examples/stories/CopyButton.stories.js
@@ -0,0 +1,27 @@
+import { fn } from '@storybook/test';
+import TwixtCopyButton from '../../react/src/CopyButton.js';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Common/TwixtCopyButton',
+  component: TwixtCopyButton,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const copyProps = {
+  args: {
+    data: 'your content is copied',
+    btnName: 'Copy button'
+  },
+};
+
