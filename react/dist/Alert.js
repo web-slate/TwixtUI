2f88c3099529d1b43ffcb6bc2e4a9686623695a4
Venkat.R <ramsunvtech@gmail.com>
2024-05-14 07:08:32 +0800

    twixt-ui-react: update story book

--- react/dist/Alert.js
+++ react/dist/Alert.js
@@ -12,9 +12,9 @@ function TwixtAlert(_ref) {
     desc = _ref.desc;
   var alertClasses = overwriteClass ? overwriteClass : 'bg-orange-100 border-l-4 border-orange-500 text-orange-700 mx-4 p-2';
   return /*#__PURE__*/_react["default"].createElement("div", {
-    "class": alertClasses,
+    className: alertClasses,
     role: "alert"
   }, /*#__PURE__*/_react["default"].createElement("p", {
-    "class": "font-bold"
+    className: "font-bold"
   }, title), /*#__PURE__*/_react["default"].createElement("p", null, desc));
 }
