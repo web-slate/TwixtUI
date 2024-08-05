ba5a3c541404c073fc74679bdb67c14c1164c5c2
Venkat.R <ramsunvtech@gmail.com>
2024-08-06 01:06:31 +0800

    fix(tailwind): adapt tailwind library level

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
+  },
+  corePlugins: {
+    preflight: false, // Disable Tailwind's base styles
   },
   plugins: [],
 }
