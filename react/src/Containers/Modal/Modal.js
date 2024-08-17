13ec9a12ede71b6e3063f26a347c8bf4b1223dd4
praveenmahesh06 <praveenmahesh06@gmail.com>
2024-08-17 15:17:37 +0530

    Containers and Stack container component Structure

---
+++ react/src/Containers/Modal/Modal.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Modal() {
+  return <>Modal goes here.</>
+}
+
+export default Modal
---
+++ react/src/Containers/SplitPane/SplitPane.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SplitPane() {
+  return <>SplitPane goes here.</>
+}
+
+export default SplitPane
