2fd227b9ac6ccb1a3631fba0f256862c5cbc0cf1
praveenmahesh06 <praveenmahesh06@gmail.com>
2024-08-18 18:37:32 +0530

    Link Component

--- react/src/CallsToAction/Link/Link.js
+++ react/src/CallsToAction/Link/Link.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-function Link() {
-  return <>Link goes here.</>
+export default function TwixtLink({
+  background = 'bg-indigo-500',
+  color = 'text-white',
+  label = 'Click Me',
+  value = '', 
+  onClick,
+  overwriteClass = '',
+}) {
+  const buttonClasses = overwriteClass || 'px-4 py-2 rounded-md';
+
+  return (
+    <a
+      href={value}
+      className={`flex items-center justify-center space-x-2 ${background} ${color} ${buttonClasses}`}
+      onClick={onClick}
+      target="_blank"
+    >
+      {label}
+    </a>
+  );
 }
-
-export default Link
