f3c41b4fcbdccf36508e24ae380eca092f74a3ce
alameluai <alamelu@nexusoceanai.com>
2024-08-18 15:45:01 +0800

    feat(call-to-action): add button component

--- react/src/CallsToAction/Button/Button.js
+++ react/src/CallsToAction/Button/Button.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-function Button() {
-  return <>Button goes here.</>
-}
+export default function TwixtButton({ children, onClick, overwriteClass }) {
+  const buttonClasses = overwriteClass ? overwriteClass : 'px-4 py-2 bg-indigo-500 text-white rounded-md';
 
-export default Button
+  return (
+    <button onClick={onClick} className={buttonClasses}>
+      {children}
+    </button>
+  );
+}
