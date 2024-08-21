29b4fd573a876a5efdb8737771bbfde8a7795930
praveenmahesh06 <praveenmahesh06@gmail.com>
2024-08-21 20:56:19 +0530

    Added Link Component

--- react/src/CallsToAction/Link/Link.js
+++ react/src/CallsToAction/Link/Link.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-function Link() {
-  return <>Link goes here.</>
+export default function TwixtLink({
+  background = '',
+  color = 'text-blue-500',
+  link = '#', 
+  onClick,
+  overwriteClass = '',
+  label,
+  leftIcon, 
+  rightIcon, 
+  target,
+}) {
+  if (!label) {
+    return null;
+  }
+
+  return (
+    <a
+      href={link}
+      className={`flex items-center gap-4 ${background} ${color} ${overwriteClass}`}
+      onClick={onClick}
+      target={target}
+    >
+      {leftIcon && <span>{leftIcon}</span>}
+      <span>{label}</span>
+      {rightIcon && <span>{rightIcon}</span>}
+    </a>
+  );
 }
-
-export default Link
