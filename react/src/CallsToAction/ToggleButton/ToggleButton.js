d678fa617476e80827683eff5802e78262544a2f
adhiththiyan <adhiththiyan07@example.com>
2024-08-24 11:57:58 +0530

    ToggleButton component added

--- react/src/CallsToAction/ToggleButton/ToggleButton.js
+++ react/src/CallsToAction/ToggleButton/ToggleButton.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function ToggleButton() {
-  return <>ToggleButton goes here.</>
+function ToggleButton({ onIcon, offIcon, overwriteClass}) {
+    const [toggle, setToggle] = useState(true)
+
+    const buttonClasses = overwriteClass || 'px-2 py-2 rounded-md cursor-pointer';
+    const handleToggle =(e)=>{
+        setToggle(!toggle)
+    }
+  return(
+    <div className={`${buttonClasses}`} onClick={handleToggle}>
+        {toggle ? onIcon: offIcon}
+    </div>
+  )
 }
-
 export default ToggleButton
