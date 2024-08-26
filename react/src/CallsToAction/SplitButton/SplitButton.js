e68d53003e759554b9cc3aae0d61902d1f3cdf5f
alameluai <alamelu@nexusoceanai.com>
2024-08-26 19:05:59 +0800

    feat(cta/split-button): add basic changes and story book changes

--- react/src/CallsToAction/SplitButton/SplitButton.js
+++ react/src/CallsToAction/SplitButton/SplitButton.js
@@ -1,52 +1,58 @@
 import React, { useState } from 'react';
+import ButtonGroup from '../ButtonGroup';
+import TwixtButton from '../Button';
+import TwixtIcon from '../../Icon';
 
-function SplitButton() {
+function SplitButton({ initialLabel = 'Select Action', options = [] }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-  const handleButtonClick = () => {
-    // Action for the main button
-    console.log('Create gist button clicked');
+  const handleMainAction = () => {
+    if (selectedOption && selectedOption.onClick) {
+      selectedOption.onClick();
+    }
+  };
+
+  const handleDropdownToggle = () => {
+    setIsOpen(!isOpen);
   };
 
   const handleOptionClick = (option) => {
-    // Action for the dropdown options
-    console.log(`${option} clicked`);
-    setIsOpen(false); // Close the dropdown
+    setSelectedOption(option);
+    setIsOpen(false);
   };
 
   return (
     <div className="relative inline-block">
-      {/* Main Button */}
-      <button
-        onClick={handleButtonClick}
-        className="px-4 py-2 bg-blue-500 text-white rounded-l-md hover:bg-blue-600 focus:outline-none"
-      >
-        Create Gist
-      </button>
+      {/* Button Group to hold the main and dropdown buttons */}
+      <ButtonGroup>
+        {/* Main Action Button */}
+        <TwixtButton
+          onClick={handleMainAction}
+          label={selectedOption ? selectedOption.label : initialLabel}
+        />
 
-      {/* Dropdown Toggle Button */}
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="px-2 py-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none"
-      >
-        ▼
-      </button>
+        {/* Dropdown Toggle Button */}
+        <TwixtButton
+          onClick={handleDropdownToggle}
+          hideLabel={true}
+          rightIcon={<TwixtIcon type="chevronDown" />}
+        />
+      </ButtonGroup>
 
       {/* Dropdown Menu */}
       {isOpen && (
-        <div className="absolute left-0 mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg">
-          <button
-            onClick={() => handleOptionClick('Create public gist')}
-            className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
-          >
-            Create public gist
-          </button>
-          <button
-            onClick={() => handleOptionClick('Create secret gist')}
-            className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
-          >
-            Create secret gist
-          </button>
+        <div className="absolute left-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
+          {options.map((option, index) => (
+            <TwixtButton
+              key={index}
+              onClick={() => handleOptionClick(option)}
+              label={option.label}
+              background="bg-white"
+              color="text-gray-700"
+              overwriteClass="w-full text-left px-4 py-2 hover:bg-gray-100"
+            />
+          ))}
         </div>
       )}
     </div>
