4aca2f1a3bc9c12b37d656a0715121d7b71ecb57
alameluai <alamelu@nexusoceanai.com>
2024-09-04 12:42:13 +0800

    feat(input/dropdown): add radio dropdown

--- react/src/Inputs/Radio/Radio.js
+++ react/src/Inputs/Radio/Radio.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 
-function Radio({
+const Radio = ({
   name,
   label,
   checked = false,
   disabled = false,
   radioColor = 'text-blue-600',
   overwriteClass = '',
-  position = 'left',
   description,
   onChange,
-}) {
+}) => {
   const [isChecked, setIsChecked] = useState(checked);
 
   const handleChange = (event) => {
@@ -23,13 +22,13 @@ function Radio({
 
   const id = `radio-${name}`;
 
-  const radioClasses = `w-5 h-5 shrink-0 mt-0.5 border-gray-200 rounded-full focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:focus:ring-offset-gray-800 ${radioColor} ${overwriteClass} ${disabled ? 'opacity-50' : 'cursor-pointer'}`;
+  const radioClasses = `w-5 h-5 shrink-0 mt-1 border-gray-200 rounded-full focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:focus:ring-offset-gray-800 ${radioColor} ${overwriteClass} ${disabled ? 'opacity-50' : 'cursor-pointer'}`;
 
-  const labelClasses = `text-sm text-gray-500 ${position === 'right' ? 'ms-auto' : 'ms-2'} flex items-center ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`;
+  const labelClasses = `text-sm text-gray-600 dark:text-neutral-500 ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`;
 
   return (
-    <div className={`flex space-x-2 ${position === 'right' ? 'justify-between' : ''}`}>
-      {position === 'left' && (
+    <div className="relative flex items-start py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-700">
+      <div className="flex items-center h-5 mt-1">
         <input
           id={id}
           type="radio"
@@ -39,28 +38,17 @@ function Radio({
           className={radioClasses}
           onChange={handleChange}
         />
-      )}
-      <label htmlFor={id} className={labelClasses}>
-        {label}
+      </div>
+      <label htmlFor={id} className="ms-3 mt-1.5 flex flex-col">
+        <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-300">{label}</span>
         {description && (
-          <span className="block text-sm text-gray-600 dark:text-neutral-500">
+          <span className={labelClasses}>
             {description}
           </span>
         )}
       </label>
-      {position === 'right' && (
-        <input
-          id={id}
-          type="radio"
-          name={name}
-          checked={isChecked}
-          disabled={disabled}
-          className={radioClasses}
-          onChange={handleChange}
-        />
-      )}
     </div>
   );
-}
+};
 
 export default Radio;
